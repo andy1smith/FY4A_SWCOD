03150e8d6ccf8a2a9c921b0d8103ba075db0b851
Nancy_DENG <andy1smith@yeah.net>
2026-01-29 20:12:16 +0800

    MODIS albedo test

--- main_SW_scope_nearealtime.py
+++ main_SW_scope_nearealtime.py
@@ -445,7 +445,7 @@ def compare_clear_dsw(site, sourcefile, meth='HG', sky="clear", file_dir=None, f
         sat['Time'] = pd.to_datetime(sat['Time'])
         sat = sat.set_index('Time')
         print('# of sat:', sat.shape[0])
-        for i in range(1):#sat.shape[0]):
+        for i in range(sat.shape[0]):
             Sun_Zen, local_zen, rela_azi = sat['Sun_Zen'][i], sat['Sat_Zen'][i], sat['Sun_Azi_sat'][i]
             COD_goes = 0  # Assuming COD is a column in sat_rad
             df_albedo_row = df_albedo.iloc[i].values
